01fa72458771ca6af932c499a246dc9ad5886670
MwanaNteba Yohani <yohani.mwananteba@student.vinci.be>
2024-12-22 00:11:50 +0100

    tuto: ex 2.3 & 2.3b

---
+++ tutorials/collections/src/components/Main/index.tsx
@@ -0,0 +1,23 @@
+import "./Main.css";
+import PizzaMenu from "./PizzaMenu";
+import sound from "../../assets/sounds/Infecticide-11-Pizza-Spinoza.mp3"
+
+const Main = () => {
+  return (
+    <main>
+      <p>My HomePage</p>
+      <p>
+        Because we love JS, you can also click on the header to stop / start the
+        music ; )
+      </p>
+      <audio id="audioPlayer" controls autoPlay>
+        <source src={sound} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+      <PizzaMenu />
+    </main>
+  );
+};
+
+
+export default Main;
